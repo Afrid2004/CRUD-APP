2f16f9f7f66521745f879d7e39c67d52c66b04e8
afrid2004 <mdfaisalafrid@gmail.com>
2026-02-07 00:38:14 +0600

    added dynamic copyrightyear

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <div className='footer'>
       <div className="custom-container">
-        <p className='text-center'>Copyright &copy; 2026 | All Rights Reserved by <a className='copy' href="https://faisalafrid.vercel.app/" target='_blank'>Afrid</a></p>
+        <p className='text-center'>Copyright &copy; {year} | All Rights Reserved by <a className='copy' href="https://faisalafrid.vercel.app/" target='_blank'>Afrid</a></p>
       </div>
     </div>
   )
